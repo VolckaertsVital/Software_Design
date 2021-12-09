4b19b2499189b4d887425d4784d5f5b9989494a2
Vital <vitalv@skynet.be>
2021-12-09 14:57:58 +0100

    added some code to database branch

--- MoneyTracker/src/main/java/controller/PersonAndTicketController.java
+++ MoneyTracker/src/main/java/controller/PersonAndTicketController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import model.person;
+import model.ticket;
+
 public class PersonAndTicketController implements Controller{
+
+    @Override
+    public void addTicket(ticket t) {
+
+    }
+
+    @Override
+    public void addUser(person p) {
+
+    }
+
+    @Override
+    public void removeTicket(ticket t) {
+
+    }
+
+    @Override
+    public void removePerson(person p) {
+
+    }
 }
+
