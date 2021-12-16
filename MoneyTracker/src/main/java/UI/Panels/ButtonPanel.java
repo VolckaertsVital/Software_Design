abb35c187fed69bcfb9f756a0b517e5fa0a3fb74
WengC <wengchiew@outlook.com>
2021-12-16 15:48:35 +0100

    UI part 1

--- MoneyTracker/src/main/java/UI/Panels/ButtonPanel.java
+++ MoneyTracker/src/main/java/UI/Panels/ButtonPanel.java
@@ -1,4 +1,33 @@
 package UI.Panels;
 
-public class ButtonPanel {
+import controller.PersonAndTicketController;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ButtonPanel extends JPanel {
+
+    private static PersonAndTicketController controller;
+
+    private final JTextField userName;
+    private final JLabel userCount;
+
+
+    private int userCounter = 0;
+    private int ticketCount = 0;
+
+
+    public ButtonPanel(PersonAndTicketController controller) {
+
+        this.controller = controller;
+
+        userCount = new JLabel("Users: " + userCounter);
+        userCount.setPreferredSize(new Dimension(75, 25));
+        this.add(userCount);
+        JLabel userLabel = new JLabel("Add/Remove user: ");
+        this.add(userLabel);
+        userName = new JTextField();
+        this.add(userName);
+
+    }
 }
---
+++ MoneyTracker/src/main/java/UI/Panels/ListPanel.java
@@ -0,0 +1,24 @@
+package UI.Panels;
+
+import register_entry.RegisterEntry;
+
+import javax.swing.*;
+
+public class ListPanel extends JPanel{
+
+    private static DefaultListModel<RegisterEntry> entryListModel;
+    //DefaultListModel is just a list implementation used for JList.
+    //https://stackoverflow.com/questions/13136826/defaultlistmodel-in-java
+    public ListPanel(){
+        JLabel label = new JLabel("Registrations");
+        entryListModel = new DefaultListModel<>();
+        JList<RegisterEntry> entryJlist = new JList<>(entryListModel);
+        this.add(label);
+        this.add(entryJlist);
+    }
+
+    public void addEntry(RegisterEntry entry){
+        this.entryListModel.addElement(entry);
+    }
+}
+
