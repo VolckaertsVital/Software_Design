554f96f81b8e975996cecd61f2a58ad97a638486
WengC <wengchiew@outlook.com>
2021-12-28 01:54:25 +0100

    UI part 2

--- MoneyTracker/src/main/java/UI/Panels/ButtonPanel.java
+++ MoneyTracker/src/main/java/UI/Panels/ButtonPanel.java
@@ -10,11 +10,16 @@ public class ButtonPanel extends JPanel {
     private static PersonAndTicketController controller;
 
     private final JTextField userName;
+
     private final JLabel userCount;
+    private final JLabel ticketCount;
+
+    private final JButton addUser;
+    private final JButton removeUser;
 
 
     private int userCounter = 0;
-    private int ticketCount = 0;
+    private int ticketCounter = 0;
 
 
     public ButtonPanel(PersonAndTicketController controller) {
@@ -28,6 +33,19 @@ public class ButtonPanel extends JPanel {
         this.add(userLabel);
         userName = new JTextField();
         this.add(userName);
+
+        addUser = new JButton(" + ");
+        addUser.setPreferredSize(new Dimension(25, 25));
+        this.add(addUser);
+
+        removeUser = new JButton(" - ");
+        removeUser.setPreferredSize(new Dimension(25, 25));
+        this.add(removeUser);
+
+        ticketCount = new JLabel("Tickets: " + ticketCounter);
+        ticketCount.setPreferredSize(new Dimension(75, 25));
+        this.add(ticketCount);
+
 
     }
 }
